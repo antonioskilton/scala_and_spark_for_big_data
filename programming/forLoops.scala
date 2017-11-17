1b37379517c87ce3ac1986675cc69abb7cfd4ceb
Antonio Skilton <antonio.skilton@gmail.com>
2017-11-16 18:43:56 -0800

    for loops

---
+++ programming/forLoops.scala
@@ -0,0 +1,24 @@
+for(num <- List(1,2,3)){
+  println(num)
+}
+for(num <- Array.range(0,5)){
+  println(num)
+}
+
+for(num <- Set(1,2,3)){
+  println(num)
+}
+for(num <- Range(0,10)){
+  if(num % 2 == 0){
+    println(s"$num is even")
+  }else{
+    println(s"$num is odd")
+  }
+}
+
+val names = List("John", "abe", "Cindy", "Cat")
+for(name <- names){
+  if(name.startsWith("C")){
+    println(s"$name starts with a C")
+  }
+}
